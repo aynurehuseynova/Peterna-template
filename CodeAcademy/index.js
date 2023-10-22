8723dfc92d47025d56801bcbf060ae4f9d748453
aynurehuseynova <sd7juqvay@code.edu.az>
2023-10-22 12:29:17 +0400

    js home

---
+++ CodeAcademy/index.js
@@ -0,0 +1,206 @@
+// const person ={
+// name:'AYNURE',
+// surname:'HUSEYNOVA',
+// point:55,
+// }
+
+// if (person.point>70){
+// console.log('yaxşı');
+// }
+// else if (50<person.point && person.point<70){
+//  console.log('kafi');
+// }
+// else {
+//     console.log('kecmedi');
+// }
+// if(person.name!='AYNURE'){
+//    alert('AYNURE');
+// }else (person.name ='AYNURE'){
+//     console.log('YAXSI');
+// }
+// let a=2
+// switch(a){
+// case 0:
+//     text = '0 oldu';
+//     break;
+//     case 1:
+//         text = '1 oldu';
+//         break;
+//         case 2:
+//             text = '2 oldu';
+//             break;
+//         default:
+//             text = 'oldu';
+// }
+// console.log(text);
+// switch (new Date().getDay()) {
+//     case 0:
+//       day = "Sunday";
+//       break;
+//     case 1:
+//       day = "Monday";
+//       break;
+//     case 2:
+//        day = "Tuesday";
+//       break;
+//     case 3:
+//       day = "Wednesday";
+//       break;
+//     case 4:
+//       day = "Thursday";
+//       break;
+//     case 5:
+//       day = "Friday";
+//       break;
+//     case 6:
+//       day = "Saturday";
+//   }
+// switch('word') {
+//     case 'a':
+//   case' ı':
+//     case 'o':
+//         case 'u':
+//             console.log ('qalin saitdi');
+//             break;
+//             default:
+//             console.log('ince sait');
+
+// }
+// 0 dan 100 qeder edeleri secsin sual
+//  for (let i = 0; i <=100; i++){
+//   console.log(i);
+//  }
+// for (let i = 0; i <=100; i+ +){
+//   if (i % 2 == 0)
+// }console,log('2 e bolunenler' + '')
+
+// let products = [
+//   { id: 1, name: "Ürün 1", price: 10.99, category: "Elektronik" },
+//   { id: 2, name: "Ürün 2", price: 15.49, category: "Giyim" },
+//   { id: 3, name: "Ürün 3", price: 5.99, category: "Mutfak" },
+//   { id: 4, name: "Ürün 4", price: 7.99, category: "Elektronik" },
+//   { id: 5, name: "Ürün 5", price: 12.99, category: "Giyim" },
+//   { id: 6, name: "Ürün 6", price: 9.99, category: "Mutfak" },
+//   { id: 7, name: "Ürün 7", price: 19.99, category: "Elektronik" },
+//   { id: 8, name: "Ürün 8", price: 3.99, category: "Giyim" },
+//   { id: 9, name: "Ürün 9", price: 8.49, category: "Mutfak" },
+//   { id: 10, name: "Ürün 10", price: 11.99, category: "Elektronik" },
+//   { id: 11, name: "Ürün 11", price: 14.99, category: "Giyim" },
+//   { id: 12, name: "Ürün 12", price: 17.99, category: "Mutfak" },
+//   { id: 13, name: "Ürün 13", price: 6.99, category: "Elektronik" },
+//   { id: 14, name: "Ürün 14", price: 13.49, category: "Giyim" },
+//   { id: 15, name: "Ürün 15", price: 4.99, category: "Mutfak" },
+//   { id: 16, name: "Ürün 16", price: 8.99, category: "Elektronik" },
+//   { id: 17, name: "Ürün 17", price: 22.99, category: "Giyim" },
+//   { id: 18, name: "Ürün 18", price: 9.49, category: "Mutfak" },
+//   { id: 19, name: "Ürün 19", price: 16.99, category: "Elektronik" },
+//   { id: 20, name: "Ürün 20", price: 7.49, category: "Giyim" }
+// ];
+
+
+// let elektronikProducts = []
+// for (let i = 0; i < products.length; i++) {
+//  if (products[i].category =='Elektronik'){
+//   elektronikProducts.push()
+//  }
+  
+// } 
+// console.log(elektronikProducts)
+
+
+
+// yuvarlaqlasdirma 100 de 1 qeder
+// let a = 1.5678968454300;
+// console.log(a.toFixed(4));
+
+
+let products = [
+    { id: 1, name: "Ürün 1", price: 10.99, category: "Elektronik" },
+    { id: 2, name: "Ürün 2", price: 15.49, category: "Giyim" },
+    { id: 3, name: "Ürün 3", price: 5.99, category: "Mutfak" },
+    { id: 4, name: "Ürün 4", price: 7.99, category: "Elektronik" },
+    { id: 5, name: "Ürün 5", price: 12.99, category: "Giyim" },
+    { id: 6, name: "Ürün 6", price: 9.99, category: "Mutfak" },
+    { id: 7, name: "Ürün 7", price: 19.99, category: "Elektronik" },
+    { id: 8, name: "Ürün 8", price: 3.99, category: "Giyim" },
+    { id: 9, name: "Ürün 9", price: 8.49, category: "Mutfak" },
+    { id: 10, name: "Ürün 10", price: 11.99, category: "Elektronik" },
+    { id: 11, name: "Ürün 11", price: 14.99, category: "Giyim" },
+    { id: 12, name: "Ürün 12", price: 17.99, category: "Mutfak" },
+    { id: 13, name: "Ürün 13", price: 6.99, category: "Elektronik" },
+    { id: 14, name: "Ürün 14", price: 13.49, category: "Giyim" },
+    { id: 15, name: "Ürün 15", price: 4.99, category: "Mutfak" },
+    { id: 16, name: "Ürün 16", price: 8.99, category: "Elektronik" },
+    { id: 17, name: "Ürün 17", price: 22.99, category: "Giyim" },
+    { id: 18, name: "Ürün 18", price: 9.49, category: "Mutfak" },
+    { id: 19, name: "Ürün 19", price: 16.99, category: "Elektronik" },
+    { id: 20, name: "Ürün 20", price: 7.49, category: "Giyim" }
+  ];
+  
+// E cataqoriya adi olanlari consolda yaz
+  // products=[]
+  // for (let x of products) {
+  //  if (x.category,startswith('E')){
+  //   console.log(x) 
+  //  }
+  // }
+
+// for (letx of products) {
+//   if (XMLDocument.category.charAt(0)=='E'){
+//  console.log(x) ;
+// }
+// }
+
+
+
+// category Giyim olanlari tapin ve Catagory adlari her biri boyuk heriflerle yazilsin
+// for (let x of products){
+//   if (x.category=='Giyim'){
+//     console.log(x,category.toUpper.Case()) ;
+//   }
+// }
+
+// for(let i = 0;i<products.length;i++){
+//   if(products[i].category == 'Giyim'){
+//     console.log(products[i].category.toUpperCase());
+//   }
+// }
+
+// bir dizideki en boyuk sayiyi bulma:
+// let numbers =[42,17,8,94,23,54]
+// let max=numbers[0];
+// for (let i = 0; i < numbers.length; i++) {
+//   if(numbers[i]>max){
+//     console,log(numbers[i]);
+//   }
+// }
+
+
+// cemi 10 dan boyukdur ya kicik oldugunu mueyyen edin 
+// let numbers=[2,4,6,8,10]
+// let sum =0;
+// for (let i = 0; i < numbers.length; i++) {
+//  sum += numbers[i];
+// }
+// console.log(sum);
+
+// if(sum >10){
+//   console.log('cem 10-dan boyukdur');
+// }
+// else if(sum<10){
+//   console.log('cem 10-dan kicikdir');
+// }
+
+
+let peopleStudents = []
+ for (let i = 0; i < students.length; i++){
+  if
+ }
+
+
+
+
+
+
+
+
